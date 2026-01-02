4e9ce19ac3343dbe6d94fe3af60df9241f47c8ab
ettoreveronese <everonese50@gmail.com>
2026-01-02 17:43:52 +0000

    update a classe Vehicle

---
+++ src/Objects/Vehicle.h
@@ -0,0 +1,20 @@
+#ifndef VEHICLE_H
+#define VEHICLE_H
+
+#include <string>
+
+class Vehicle {
+private:
+    const std::string plate;
+
+public:
+    Vehicle();                                          // generate a new plate if none is given
+    explicit Vehicle(const std::string& p);
+    
+    const std::string& getPlate() const;                // returns the plate of the current vehicle
+    std::string makePlate() const;                      // returns a newly generated plate
+    bool isValidPlate(const std::string& plate) const;  // returns true if the given plate is valid    
+};
+
+
+#endif
