4307febedddd738a29d3bf979abf3c372a10311a
paologas <paolo.gas05@gmail.com>
2026-01-04 16:26:34 +0100

    Update Gantry.cpp

--- common/Gantry.cpp
+++ common/Gantry.cpp
@@ -3,10 +3,10 @@
 
 Gantry::Gantry(const double& dist_gantry, const int& num_gantry) : dist_gantry(dist_gantry), num_gantry(num_gantry) {}
 
-double Gantry::getDist() const{
+double Gantry::getGantryDist() const{
 	return dist_gantry;                                 // fornisce la distanza in km dall'inizio
 }
 
-int Gantry::getNum() const{
+int Gantry::getGantryNum() const{
 	return num_gantry;                                   // fornisce il # del varco
 }
