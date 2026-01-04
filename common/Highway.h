8e217f2b2aaf195f908a759d1cc7381cc50a09eb
paologas <paolo.gas05@gmail.com>
2026-01-04 16:30:28 +0100

    Update Highway.h

--- common/Highway.h
+++ common/Highway.h
@@ -10,13 +10,14 @@
 
 class Highway{
 private:
-	std::vector<Gantry> gantries;
-	std::vector<Junction> junctions;
+	std::vector<Gantry> gantries;				// vettore per i varchi
+	std::vector<Junction> junctions;			// vettore per gli svincoli
 	
 	void isValid() const;			// controlla i vincoli (almeno 2 varchi, distanza minima di 1km,...)
 public:
-	void loadHighwayData(const std::string& h_data);      // prende i dati da Highway.txt 
+	void loadHighwayData(const std::string& h_data);      // prende i dati da Highway.txt (nel file main)
 	const std::vector<Gantry>& getGantries() const;
 	const std::vector<Junction>& getJunctions() const;
 }
 #endif
+
