01e2fadf3c4913da1ad3c50dca677fc22248dfde
paologas <paolo.gas05@gmail.com>
2026-01-04 16:20:01 +0100

    Update Gantry.cpp

--- common/Gantry.cpp
+++ common/Gantry.cpp
@@ -1,7 +1,7 @@
 // Autore : Paolo Gasparato
 #include "Gantry.h"
 
-Gantry::Gantry(const int& gantry_id, const double& dist) : id(gantry_id), dist(dist) {}
+Gantry::Gantry(const double& gantry_dist, const int& num_g) : id(gantry_id), dist(dist) {}
 
 int Gantry::getId() const{
 	return id;                                   // fornisce il # del varco
