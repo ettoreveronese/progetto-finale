8ec8554da94229e015da55e79d1c94f73e8ac9df
paologas <paolo.gas05@gmail.com>
2026-01-13 15:44:22 +0100

    Update Junction.cpp

--- common/Junction.cpp
+++ common/Junction.cpp
@@ -1,13 +1,14 @@
 // Autore : Paolo Gasparato
 #include "Junction.h"
 
-Junction::Junction(const double& dist_junction, const int& num_junction) : dist_junction(dist_junction), num_junction(num_junction) {}
+Junction::Junction(const double& junction_dist, const int& junction_num) : junction_dist(junction_dist), junction_num(junction_num) {}
 
 double Junction::get_junction_dist() const{
-	return dist_junction;                                 // fornisce la distanza in km dall'inizio
+	return junction_dist;                                 // fornisce la distanza in km dall'inizio
 }
 
 int Junction::get_junction_num() const{
-	return num_junction;                                   // fornisce il # del varco
+	return junction_num;                                   // fornisce il # del varco
 }
+
 
