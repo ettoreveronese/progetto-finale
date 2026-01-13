40545387fdd42df632ef9e85738976938f3580b6
paologas <paolo.gas05@gmail.com>
2026-01-13 15:49:30 +0100

    Update Junction.cpp

--- common/Junction.cpp
+++ common/Junction.cpp
@@ -3,13 +3,14 @@
 
 Junction::Junction(const double& junction_dist, const int& junction_num) : junction_dist(junction_dist), junction_num(junction_num) {}
 
-double Junction::get_dist() const{
+double Junction::get_junction_dist() const{
 	return junction_dist;                                 // fornisce la distanza in km dall'inizio
 }
 
-int Junction::get_num() const{
+int Junction::get_junction_num() const{
 	return junction_num;                                   // fornisce il # del varco
 }
+
 
 
 
