f42d488fc28e5c93209dbc7cf85a3b4fe8aeb86f
paologas <paolo.gas05@gmail.com>
2026-01-13 15:46:07 +0100

    Update Gantry.cpp

--- common/Gantry.cpp
+++ common/Gantry.cpp
@@ -1,12 +1,12 @@
 // Autore : Paolo Gasparato
 #include "Gantry.h"
 
-Gantry::Gantry(const double& gantry_dist, const int& gantry_id) : gantry_dist(gantry_dist), gantry_id(gantry_id) {}
+Gantry::Gantry(const double& gantry_dist, const int& gantry_num) : gantry_dist(gantry_dist), gantry_num(gantry_num) {}
 
 double Gantry::get_dist() const{
-	return gantry_id;                                 // fornisce la distanza in km dall'inizio
+	return gantry_dist;                                 // fornisce la distanza in km dall'inizio
 }
 
-int Gantry::get_id() const{
-	return gantry_id;                                   // fornisce il # del varco
+int Gantry::get_num() const{
+	return gantry_num;                                   // fornisce il # del varco
 }
