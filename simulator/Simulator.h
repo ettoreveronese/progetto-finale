969341574f6b2e93712eb991c98a43ab16336834
Tommasopolo <tommasopolo06@gmail.com>
2026-01-15 16:35:47 +0100

    Update Simulator.h

--- simulator/Simulator.h
+++ simulator/Simulator.h
@@ -49,11 +49,12 @@ double dist(int speed, int duration);          //Calculate distance traveled in 
 
 Vehicle gen_v(double dist, int num_junction, double t_start);  //Generate vehicle
 
-void run(const Vehicle &v, ofstream &file);       //Write vehicle to file
+void run(const Vehicle &v, std::ofstream &file);       //Write vehicle to file
 
-void passage(const Vehicle &v, ofstream &file, double distTot);  ////Write the passed gates and at what time to the file
+void passage(const Vehicle &v, std::ofstream &file, double distTot);  ////Write the passed gates and at what time to the file
 
 #endif
+
 
 
 
