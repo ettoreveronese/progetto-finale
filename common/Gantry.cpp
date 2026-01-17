33079fc8288c53691ea03a91aede45d639d6b8a0
paologas <paolo.gas05@gmail.com>
2026-01-17 14:47:43 +0100

    Update Gantry.cpp

--- common/Gantry.cpp
+++ common/Gantry.cpp
@@ -1,7 +1,7 @@
 // Autore : Paolo Gasparato
 #include "Gantry.h"
 
-Gantry::Gantry(const double& gantry_dist, const int& gantry_id) : gantry_dist(gantry_dist), gantry_id(gantry_id) {}
+Gantry::Gantry(const int& gantry_id, const double& gantry_dist) : gantry_id(gantry_id), gantry_dist(gantry_dist) {}
 
 double Gantry::get_dist() const{
 	return gantry_dist;                                 // fornisce la distanza in km dall'inizio
