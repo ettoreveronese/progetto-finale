d2b1b106982e8de412963dfbad2b751f0206932a
paologas <paolo.gas05@gmail.com>
2026-01-17 14:49:54 +0100

    Update Junction.h

--- common/Junction.h
+++ common/Junction.h
@@ -8,11 +8,12 @@ private:
 	double junction_dist;				// distanza (in km)
 	int junction_id;				    // # dello svincolo
 public:
-	Junction(const int& junction_id. const double& junction_dist);
+	Junction(const int& junction_id, const double& junction_dist);
 		double get_dist() const;	
 		int get_id() const;
 };
 #endif
+
 
 
 
